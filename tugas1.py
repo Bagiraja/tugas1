8e3b4f8718881209aff3fca9513a6a9738899020
Bagiraja <bagirmustofa93@gmail.com>
2025-10-21 22:39:07 +0700

    update codingan

--- tugas1.py
+++ tugas1.py
@@ -1,46 +1,54 @@
-Nama_karyawan = input("Masukan Nama Karyawan: ")
-Jabatan = input('Pilih Jabatan [1/2/3]: ')
-pendidikan = input('Pilih Pendidikan [SMA/D1/D3/S1]: ')
-jam_kerja = int(input('Masukan Jam Kerja:'))
+print("PROGRAM HITUNG GAJI KARYAWAN")
+print("------------------------------------")
 
+nama = input("Nama Karyawan     : ")
+golongan = input("Golongan Jabatan  [1/2/3] : ")
+pendidikan = input("Pendidikan [SMA/D1/D3/S1] : ")
+jamkerja = int(input("Jumlah Jam Kerja  : "))
+
+# Proses Gaji Pokok berdasarkan Golongan
 gaji_pokok = 300000
 
-if Jabatan == '1':
-    tuj_jabatan = 0.05 *    gaji_pokok
-elif Jabatan == '2':
-    tuj_jabatan = 0.10 * gaji_pokok
-elif Jabatan == '3':
-    tuj_jabatan = 0.30 * gaji_pokok
+# Proses Tunjangan Jabatan
+if golongan == "1":
+    tj_jabatan = 0.05 * gaji_pokok
+elif golongan == "2":
+    tj_jabatan = 0.15 * gaji_pokok
+elif golongan == "3":
+    tj_jabatan = 0.30 * gaji_pokok
 else:
-    tuj_jabatan = 0
-    print('Jabatan Tidak Valid')
+    tj_jabatan = 0
+    print("Golongan tidak dikenal, tunjangan jabatan = 0")
 
-if pendidikan == 'SMA' or 'sma':
-   tunj_pendidikan =  0.025 * gaji_pokok
-elif pendidikan == 'D1':
-    tunj_pendidikan = 0.05 * gaji_pokok
-elif pendidikan == 'D3':
-    tunj_pendidikan = 0.20 * gaji_pokok
-elif pendidikan == 'S1':
-    tunj_pendidikan = 0.30 * gaji_pokok
+# Proses Tunjangan Pendidikan
+if pendidikan.upper() == "SMA":
+    tj_pendidikan = 0.025 * gaji_pokok
+elif pendidikan.upper() == "D1":
+    tj_pendidikan = 0.05 * gaji_pokok
+elif pendidikan.upper() == "D3":
+    tj_pendidikan = 0.20 * gaji_pokok
+elif pendidikan.upper() == "S1":
+    tj_pendidikan = 0.30 * gaji_pokok
 else:
-    tunj_pendidikan = 0
-    print('Pendidikan Tidak Valid')
+    tj_pendidikan = 0
+    print("Pendidikan tidak dikenal, tunjangan pendidikan = 0")
 
-if jam_kerja > 8:
-    lembur = (jam_kerja - 8) * 3500
+# Proses Honor Lembur
+if jamkerja > 8:
+    lembur = (jamkerja - 8) * 3500
 else:
     lembur = 0
 
-total_gaji = gaji_pokok + tuj_jabatan + tunj_pendidikan + lembur
-print('==============================')
-print("===nh=")
-print('Nama Karyawan :', Nama_karyawan)
-print('Golongan Jabatan :', Jabatan)
-print('Pendidikan :', pendidikan)
-print('Jam Kerja :', jam_kerja)
-print('Gaji Pokok :', gaji_pokok)
-print('Tunjungan Jabatan :', tuj_jabatan )
-print('Tunjungan Pendidikan :', tunj_pendidikan )
-print('Lembur :', lembur)
-print('Total Gaji :', total_gaji )
+# Total Gaji
+total_gaji = gaji_pokok + tj_jabatan + tj_pendidikan + lembur
+
+# Layar Keluaran
+print("\n------------------------------------")
+print("Karyawan yang bernama :", nama)
+print("Honor yang diterima")
+print("Tunjangan Jabatan     : Rp", int(tj_jabatan))
+print("Tunjangan Pendidikan  : Rp", int(tj_pendidikan))
+print("Honor Lembur          : Rp", int(lembur))
+print("------------------------------------")
+print("Total Gaji            : Rp", int(total_gaji))
+print("------------------------------------")
